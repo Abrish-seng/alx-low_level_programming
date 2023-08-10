747c66f38c4463602f9409342bea0371aadff055
abrish-seng <abrish814@gmail.com>
2023-08-10 06:12:06 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -a program that points a new line with a puts function
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

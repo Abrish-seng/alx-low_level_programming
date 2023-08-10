9ef6a0fad0a407341dc2c58ca82af920ed88ecd6
abrish-seng <abrish814@gmail.com>
2023-08-10 06:22:53 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/** 
+ * main function with a printf line
+ * Return :0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

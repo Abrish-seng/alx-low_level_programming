5aca34d5d68b25b040069f762181e33f4890d0c6
abrish-seng <abrish814@gmail.com>
2023-08-11 04:34:11 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Prints the alphabet in lowercase, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number <= 10; ++number)
+	{
+		
+		putchar(number);
+
+	}
+	putchar('\n');
+	return (0);
+}

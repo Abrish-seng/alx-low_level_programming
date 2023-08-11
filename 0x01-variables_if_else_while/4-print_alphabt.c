c3e53125a75b40d8c74498de4677c5f0a9b3482e
abrish-seng <abrish814@gmail.com>
2023-08-11 04:22:13 -0700

    4-print-alphabet.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,13 +9,13 @@
 int main(void)
 {
 	char letter;
-if(letter = 'e' && letter = 'q'){
 
 	for (letter = 'a'; letter <= 'z'; ++letter)
 	{
+           if(letter != 'e' && letter != 'q'){
 		putchar(letter);
-	}
-}
+	    } 
+        }
 	putchar('\n');
 
 	return (0);

c7de7551811543c77c1829b2d2cd96c483fa01bf
Abrhaley Gebreslassie <38569122+Abrish-seng@users.noreply.github.com>
2023-08-18 01:06:41 +0300

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,16 @@
-
 #include "main.h"
-
 /**
- * print_numbers - prints 0 to 9.
- * by abrish
- */
+*print_numbers - function that print all numbers from
+*0 to 9
+*
+*Return: returns nothing
+*/
 void print_numbers(void)
 {
-	int num = 0;
-
-	while (num < 10)
-	{
-		_putchar('0' + num);
-		num++;
-	}
-	_putchar('\n');
+int number = 48;
+for (number = 48; number < 58; number++)
+{
+_putchar(number);
+}
+_putchar(10);
 }

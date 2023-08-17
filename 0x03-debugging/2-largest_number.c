31c5c9175d58e34a1fa854fd7959cf95dac5d168
abrish-seng <abrish814@gmail.com>
2023-08-17 03:27:48 -0700

    done

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -15,11 +15,23 @@ if (a >= b && a >= c)
 {
 largest = a;
 }
+else if (a  >= c && c >= b)
+{
+largest = a;
+}
 else if (b >= a && b >= c)
 {
 largest = b;
 }
-else
+else if (b >= c && c >= a)
+{
+largest = b;
+}
+else if (c >= a && c >= b)
+{
+largest = c;
+}
+else if (c >= b && b >= a)
 {
 largest = c;
 }

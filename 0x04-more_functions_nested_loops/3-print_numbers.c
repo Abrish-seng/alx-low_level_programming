e7de72b5cfcf140fa84317fbb5dc61f5b2a3abe0
abrish-seng <abrish814@gmail.com>
2023-08-18 04:31:29 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,10 +7,10 @@
 */
 void print_numbers(void)
 {
-int number = 48;
-for (number = 48; number < 58; number++)
+int number = 0;
+for (number = 0; number < 10; number++)
 {
 _putchar(number);
 }
-_putchar(10);
+_putchar("\n");
 }

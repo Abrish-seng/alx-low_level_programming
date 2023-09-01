fac4dea95ff92044397ef27c153a8be25e00133c
abrish-seng <abrish814@gmail.com>
2023-09-01 10:49:21 -0700

    counter is building

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
-
 /**
- * main - Write a program that prints its name, followed by a new line.
- * If you rename the program, it will print the new name, without having to
- * compile it again
- * You should not remove the path before the name of the program
+ * main - Write a program that prints the number of arguments passed into it
  *
  * @args: This is the argument count
  * @argv: This is the argument vector
  *
  * Return: This return to 0
+ *
  */
 int main(int args, char *argv[])
 {
 	printf("%d\n", args - 1);
-       (void) argv; 
+	(void)argv;
 	return (0);
 }

3eb83c99b70e37f94f9d7b12b6c339911db03740
abrish-seng <abrish814@gmail.com>
2023-09-01 10:46:34 -0700

    argument counter

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,14 +6,14 @@
  * compile it again
  * You should not remove the path before the name of the program
  *
- * @argc: This is the argument count
+ * @args: This is the argument count
  * @argv: This is the argument vector
  *
  * Return: This return to 0
  */
-int main(int argc, char *argv[])
+int main(int args, char *argv[])
 {
-	printf("%d\n", argc - 1);
+	printf("%d\n", args - 1);
        (void) argv; 
 	return (0);
 }

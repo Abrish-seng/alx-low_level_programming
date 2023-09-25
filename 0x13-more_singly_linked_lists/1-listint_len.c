d7bf95a618ef95ec70b2e3d2d6d7e2e2c40cc265
abrish-seng <abrish814@gmail.com>
2023-09-25 11:23:10 -0700

    a program that counts the number of nodes listed

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list
+ * @h: pointer to the list_t list
+ *
+ * Return: number of elements in h
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t n = 0;
+
+	while (h)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
+}

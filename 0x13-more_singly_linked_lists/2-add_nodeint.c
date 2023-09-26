3985ed8307081fb05a29381b6b55aa2b4c5aecea
abrish-seng <abrish814@gmail.com>
2023-09-26 06:55:16 -0700

    betty error fixed

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,7 +1,6 @@
 #include "lists.h"
 
 /**
- *
  * add_nodeint - adds a new node at the beginning of the  linked list
  * @head: pointer to the first node in the linked list
  * @n: data to insert in that new node

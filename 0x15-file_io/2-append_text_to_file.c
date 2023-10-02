81970e8eb413ed0148c6640545443b859f94a439
abrish-seng <abrish814@gmail.com>
2023-10-02 12:48:12 -0700

    appending text to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -4,7 +4,6 @@
 
 /**
  * append_text_to_file - this function appends the file
- * 
  * @filename: variable pointer of the file
  * @text_content: content file
  * Return: 1 when it is success, and -1 when it failing
@@ -12,22 +11,26 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int i = 0, file;
+	int i = 0, file;
 
-    if (filename == NULL)
-        return (-1);
-    if (text_content == NULL)
-        text_content = "";
-    
-    while (text_content[i] != '\0')
-    {
-        i++;
+	if (filename == NULL)
+		return (-1);
 
-    }
-    file = open(filename, o_WRONLY | o_APPEND);
+	if (text_content == NULL)
+		text_content = "";
 
-    if (file == -1)
-        return (-1);
-    write(file,text_content,i);
-    return (1);
+
+	while (text_content[i] != '\0')
+	{
+		i++;
+	}
+
+	file = open(filename, O_WRONLY | O_APPEND);
+
+	if (file == -1)
+		return (-1);
+
+	write(file, text_content, i);
+
+	return (1);
 }
